fb79e0a3bdcc4f9e8df768275eab73c549293825
alumne <alumne@a302-31>
2018-05-16 10:46:11 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -15,6 +15,8 @@ public class Prjava01 {
 
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");
+            bw.newLine();
             bw.write("<html>");
             bw.newLine();
             bw.write("  <head>");
